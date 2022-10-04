6f33707705591c7dbf6d8b88a83808c0d7f4c6b5
Kai Xiang (Neo) Zhang <neo.cyc@gmail.com>
2022-10-04 14:28:08 -0700

    Done: Enemy, Enemy Collision, Player Movement

--- docs/OneButton/main.js
+++ docs/OneButton/main.js
@@ -3,16 +3,43 @@ title = "One Button";
 description = `
 `;
 
-characters = [];
+characters = [
+`
+llllll
+llllll
+llllll
+llllll
+llllll
+llllll
+`,`
+bbbbbb
+bbbbbb
+bbbbbb
+bbbbbb
+bbbbbb
+bbbbbb
+`,`
+rrrrrr
+rrrrrr
+rrrrrr
+rrrrrr
+rrrrrr
+rrrrrr
+`
+];
 
 const G = {
 	WIDTH: 100,
-	HEIGHT: 150
+	HEIGHT: 150,
+	ENEMY_SPEED: 1.0
 };
 
 options = {
 	viewSize: {x: G.WIDTH, y: G.HEIGHT}
 };
+
+//======== GAME FUNCTION ========//
+
 
 //======== GAME OBJECTS ========//
 /**
@@ -28,31 +55,87 @@ options = {
  */
 let player;
 
+/**
+ * @typedef {{
+ * pos: Vector,
+ * speed: number
+ * }} Enemy
+ */
+
+/**
+ * @type { Enemy [] }
+ */
+let enemies;
+
+/**
+ * @type { number }
+ */
+ let waveCount;
+
 
 //======== GAME  CODES ========//
 
 function update() {
 	if (!ticks) {
+		waveCount = 0;
+
+		enemies = times(3, () => {
+			const posX = 10 * (rndi(0, 5) * 2 + 1);
+			const posY = 0;
+			return {
+				pos: vec(posX, posY),
+				speed: 2
+			}
+		});
 		player = {
-			pos: vec(G.WIDTH * 0.5, G.HEIGHT * 0.75),
-			speed: 1,
+			pos: vec(G.WIDTH * 0.5, G.HEIGHT * 0.85),
+			speed: 1.5,
 			direction: true
 		};
 	}
 
-	if (input.isJustPressed) {
-		player.direction = !player.direction;
-	}
-	if (player.direction) {
-		color("red");
-		player.pos.x += player.speed;
-	}
-	if (!player.direction) {
-		color("blue");
-		player.pos.x -= player.speed;
+	// Update/Spawn the Enemies
+	if (enemies.length === 0) {
+		for (let i = 0; i < 3; i++) {
+			const posX = 10 * (rndi(0, 5) * 2 + 1);
+			const posY = 0;
+			enemies.push({
+				pos: vec(posX, posY),
+				speed: 2
+			});
+		}
+		waveCount++;
 	}
 
-	player.pos.clamp(5, G.WIDTH - 5, 0, G.HEIGHT);
+	remove(enemies, (e) => {
+		e.pos.y += e.speed;
+		color("black");
+		char("a", e.pos);
 
-	box(player.pos, 3);
+		return (e.pos.y > G.HEIGHT);
+	})
+	// enemies.forEach((e) => {
+	// 	e.pos.y += e.speed;
+	// 	if (e.pos.y > G.HEIGHT) {
+	// 		e.pos.x = 
+	// 		e.pos.y = 0;
+	// 	}
+	// 	color("black");
+	// 	char("a", e.pos);
+	// });
+
+	if (input.isJustPressed) player.direction = !player.direction;
+
+	if (player.direction)  player.pos.x += player.speed;
+	if (!player.direction) player.pos.x -= player.speed;
+
+	player.pos.clamp(10, G.WIDTH - 10, 0, G.HEIGHT);
+	color("black");
+	char("b", player.pos);
+
+	const isCollidingWithPlayer = char("b", player.pos).isColliding.char.a;
+	if (isCollidingWithPlayer) {
+		end();
+		play("powerUp");
+	}
 }
