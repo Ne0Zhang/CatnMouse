5f3eb7c5cf77f85c9a9dad304c758cfceae26c8c
Kai Xiang (Neo) Zhang <neo.cyc@gmail.com>
2022-10-05 16:42:44 -0700

    Changes

--- docs/catnmouse/main.js
+++ docs/catnmouse/main.js
@@ -1,4 +1,4 @@
-title = "Cat and Mouse";
+title = "Cat-n-Mouse";
 
 description = `
 `;
@@ -12,28 +12,35 @@ LlLLlL
  LLLL
   ll
 `,`
-bbbbbb
-bbbbbb
-bbbbbb
-bbbbbb
-bbbbbb
-bbbbbb
+  LL  
+ lLLl
+RLLLLR
+RLLLLR
+ LLLL
+ LLLL
 `,`
-rrrrrr
-rrrrrr
-rrrrrr
-rrrrrr
-rrrrrr
-rrrrrr
+    RR
+ lLlRR
+ LLLL 
+ lLLL
+RRLLL
+RR  RR
+`,`
+RR
+RRlLl
+ LLLL
+ LLLl
+ LLLRR
+RR  RR
 `
 ];
 
 const G = {
-	WIDTH: 100,
-	HEIGHT: 150,
-	MIN_X: 10,
-	MAX_X: 90,
-	ENEMY_SPEED: 1.0
+	WIDTH: 58,
+	HEIGHT: 87,
+	MIN_X: 2,
+	MAX_X: 56,
+	ENEMIES: 7
 };
 
 options = {
@@ -86,17 +93,17 @@ function update() {
 		waveCount = 0;
 		arrayPos = [];
 
-		enemies = times(3, () => {
-			const posX = G.MIN_X * (rndi(0, 5) * 2 + 1);
+		enemies = times(G.ENEMIES - 3, () => {
+			const posX = (rndi(0, G.ENEMIES) * 8) + 5;
 			const posY = 0;
 			return {
 				pos: vec(posX, posY),
-				speed: 1
+				speed: 0.5
 			}
 		});
 		player = {
 			pos: vec(G.WIDTH * 0.5, G.HEIGHT * 0.85),
-			speed: 1.5,
+			speed: 0.6,
 			direction: true
 		};
 	}
@@ -104,11 +111,11 @@ function update() {
 	// Update/Spawn the Enemies
 	if (enemies.length === 0) {
 		for (let i = 0; i < 3; i++) {
-			const posX = G.MIN_X * (rndi(0, 5) * 2 + 1);
+			const posX = (rndi(0, G.ENEMIES) * 8) + 5;
 			const posY = 0;
 			enemies.push({
 				pos: vec(posX, posY),
-				speed: 1 + (waveCount * 0.1)
+				speed: 0.5 + (waveCount * 0.02)
 			});
 		}
 		waveCount++;
@@ -121,21 +128,24 @@ function update() {
 		e.pos.y += e.speed;
 		color("black");
 		char("a", e.pos);
-
 		return (e.pos.y > G.HEIGHT);
 	})
 
 	// If input or player reach the border, change direction
-	if (input.isJustPressed || player.pos.x < G.MIN_X || player.pos.x > G.MAX_X) 
+	if (input.isJustPressed || player.pos.x < G.MIN_X + 2 || player.pos.x > G.MAX_X - 2) 
 		player.direction = !player.direction;
-	if (player.direction)  player.pos.x += player.speed; // Go to the Right
-	if (!player.direction) player.pos.x -= player.speed; // Go to the Left
+	if (player.direction)  {
+		player.pos.x += player.speed; // Go Right
+	}
+	if (!player.direction) {
+		player.pos.x -= player.speed; 
+	}// Go to the Left
 
-	player.pos.clamp(G.MIN_X - 1, G.MAX_X + 1, 0, G.HEIGHT);
 	color("black");
 	char("b", player.pos);
 
-	// If Player collides with an enemy, Game Over
+	// player.pos.clamp(G.MIN_X + 2, G.MAX_X - 2, 0, G.HEIGHT);
+	
 	const isCollidingWithPlayer = char("b", player.pos).isColliding.char.a;
 	if (isCollidingWithPlayer) {
 		end();
